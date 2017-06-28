479f853c1e7dcf66dbc37ec68b426a11eee7662e
Alex <kik9399@mail.ru>
2017-06-29 00:23:46 +0300

    header class Matrix ver[1.0] -- main functions

---
+++ include/Matrix.h
@@ -0,0 +1,49 @@
+#pragma once
+
+template<class T> class Matrix;
+template <typename T>
+
+class Matrix {
+private:
+	
+	unsigned int			rows_;
+	unsigned int			columns_;
+	T**						Array_;
+
+public:
+	
+	Matrix() {
+		rows_ = 0;
+		columns_ = 0;
+		Array_ = nullptr;
+	}
+	
+	Matrix(unsigned int rows, unsigned int columns) {
+		rows_ = rows;
+		columns_ = columns;
+
+		Array_ = new T*[rows];
+		for (unsigned int i = 0; i < rows; ++i) {
+			Array_[i] = new T[columns_];
+			for (unsigned int j = 0; j < columns; ++j)
+				Array_[i][j] = 0;
+		}
+	}
+	
+	Matrix(const Matrix& get) {
+		rows_ = get.rows_;
+		columns_ = get.columns_;
+		Array_ = new T*[rows_];
+		for (unsigned int i = 0; i < rows_; ++i) {
+			Array_[i] = new T[columns_];
+			for (unsigned int j = 0; j < columns_; ++j) {
+				Array_[i][j] = get.Array_[i][j];
+			}
+		}
+	}
+
+	void setElement(unsigned int row, unsigned int column, T key) {
+		Array_[row][column] = key;
+	}
+
+};
