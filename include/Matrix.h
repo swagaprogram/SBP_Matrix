6217f02e9ec85919c4038a42b45fe45342ec3f5d
Alex <kik9399@mail.ru>
2017-06-29 12:21:50 +0300

    header class Matrix ver[1.2] -- ~Matrix() and operators

--- include/Matrix.h
+++ include/Matrix.h
@@ -18,7 +18,6 @@ public:
 		columns_ = 0;
 		Array_ = nullptr;
 	}
-	
 	Matrix(unsigned int rows, unsigned int columns) {
 		rows_ = rows;
 		columns_ = columns;
@@ -30,7 +29,6 @@ public:
 				Array_[i][j] = 0;
 		}
 	}
-	
 	Matrix(const Matrix& get) {
 		rows_ = get.rows_;
 		columns_ = get.columns_;
@@ -42,12 +40,17 @@ public:
 			}
 		}
 	}
+	~Matrix() {
+		for (unsigned int i = 0; i < rows_; ++i) {
+			delete[] Array_[i];
+		}
+		delete[] Array_;
+	}
 	
-	void setElement(unsigned int row, unsigned int column, T key) {
+	void					setElement(unsigned int row, unsigned int column, T key) {
 		Array_[row][column] = key;
 	}
-
-	void print() {
+	void					print() {
 		for (unsigned int i = 0; i < rows_; ++i) {
 			for (unsigned int j = 0; j < columns_; ++j) {
 				std::cout << Array_[i][j] << " ";
@@ -56,4 +59,70 @@ public:
 		}
 	}
 
+	Matrix<T> operator+ (const Matrix& mat) const {
+		try {
+			if ((rows_ == mat.rows_) && (columns_ == mat.columns_)) {
+				Matrix getRes(rows_, columns_);
+				for (unsigned int i = 0; i < rows_; ++i)
+					for (unsigned int j = 0; j < columns_; ++j)
+						getRes.Array_[i][j] = Array_[i][j] + mat.Array_[i][j];
+				return getRes;
+			}
+			else {
+				throw "Matrixes are not identical\nFirst matrix:\n";
+			}
+		}
+		catch (char *msg) {
+			std::cout << "Error: " << msg;
+			return *this;
+		}
+
+	}
+	Matrix<T> operator* (const Matrix& mat) const {
+		try {
+			if ((columns_ == mat.rows_)) {
+				Matrix getRes(rows_, mat.columns_);
+				for (unsigned int i = 0; i < rows_; ++i) {
+					for (unsigned int j = 0; j < mat.columns_; ++j)
+						for (unsigned int k = 0; k < columns_; ++k)
+							getRes.Array_[i][j] += Array_[i][k] * mat.Array_[k][j];
+				}
+				return getRes;
+			}
+			else {
+				throw "!These matrixes can't be multiplied!\nFirst matrix:\n";
+			}
+		}
+		catch (char *msg) {
+			std::cout << "Error" << msg;
+			return *this;
+		}
+	}
+	Matrix<T>& operator= (const Matrix& right) {
+		if (this == &right) {
+			return *this;
+		}
+		for (unsigned int i = 0; i < rows_; ++i) {
+			delete[]Array_[i];
+		}
+		delete[]Array_;
+		Array_ = new T*[right.rows_];
+		for (unsigned int i = 0; i < right.rows_; ++i) {
+			Array_[i] = new T[right.columns_];
+			for (unsigned int j = 0; j < right.columns_; ++j)
+				Array_[i][j] = right.Array_[i][j];
+		}
+		rows_ = right.rows_;
+		columns_ = right.rows_;
+		return *this;
+	}
+	bool operator== (const Matrix& right) const {
+		if ((rows_ == right.rows_) && (columns_ == right.columns_)) {
+			for (unsigned int i = 0; i < rows_; ++i)
+				for (unsigned int j = 0; j < columns_; ++j) {
+					if (Array_[i][j] != right.Array_[i][j]) { return false; }
+				}
+		}
+		else return false;
+	}
 };
